d6971429d809a17d2f79f0f03d7d1b67cb4d906c
Dylann02 <hopkinslucka@gmail.com>
2026-07-20 08:45:56 +0300

    test

--- script.sql
+++ script.sql
@@ -5,8 +5,3 @@ CREATE TABLE produit (
     nom VARCHAR(30),
     prix FLOAT
 );
-
-INSERT INTO produit(nom,prix) VALUES ("Burger",15000);
-INSERT INTO produit(nom,prix) VALUES ("Pizza",23000);
-INSERT INTO produit(nom,prix) VALUES ("Frites",4000);
-INSERT INTO produit(nom,prix) VALUES ("Frites",4000);
